64952cc5660cc9aa470efd8933237bdb711204cb
OwenHotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2022-09-11 21:58:26 +0000

    update

---
+++ prog54b.py
@@ -0,0 +1,10 @@
+number1 = int(input("Please enter number1: "))
+number2 = int(input("Please enter number2: "))
+number3 = int(input("Please enter number3: "))
+number4 = int(input("Please enter number4: "))
+
+sum = number1 + number2 + number3 + number4
+average = sum / 4
+
+print("The sum is", sum)
+print("The average is", average)
