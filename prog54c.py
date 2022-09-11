8ac3db8fe9acac6737b9eafe9d1be7b5212c4588
OwenHotchkiss <113038190+Owenhotchkiss@users.noreply.github.com>
2022-09-11 22:51:57 +0000

    update

---
+++ prog54c.py
@@ -0,0 +1,8 @@
+radius = float(input("Please enter radius:"))
+pi = float(3.14159)
+
+area = radius * radius * pi
+circ = 2 * radius *pi
+
+print("The area of the circle is", area)
+print("The circ of the circle is", circ)
